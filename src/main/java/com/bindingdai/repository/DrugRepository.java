0b20f65a7769b23c4fa1516c3621da3f3fa68bcc
bindingdai <bindingdai@gmail.com>
2016-08-26 22:05:52 +0800

    fix bugs

--- src/main/java/com/bindingdai/repository/DrugRepository.java
+++ src/main/java/com/bindingdai/repository/DrugRepository.java
@@ -2,7 +2,7 @@ package com.bindingdai.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.bindingdai.model.*;
+import com.bindingdai.model.DrugEntity;
 /**
  * Created by daibinding on 16/4/19.
  */
--- src/main/java/com/bindingdai/repository/clinicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bindingdai.repository;
-
-/**
- * Created by daibinding on 16/4/19.
- */
-public interface clinicRepository {
-}
