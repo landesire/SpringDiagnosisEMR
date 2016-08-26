2de7c7db67fc480226fbdfb68071f9247fd973b7
bindingdai <bindingdai@gmail.com>
2016-08-26 21:53:44 +0800

    first commit
    fix bugs

--- src/main/java/com/bindingdai/repository/DrugRepository.java
+++ src/main/java/com/bindingdai/repository/DrugRepository.java
@@ -2,12 +2,11 @@ package com.bindingdai.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.bindingdai.model.PhysicalExamEntity;
+import com.bindingdai.model.*;
 /**
  * Created by daibinding on 16/4/19.
  */
 @Repository
-public interface PhysicalExamRepository extends JpaRepository<PhysicalExamEntity,Integer>{
+public interface DrugRepository extends JpaRepository<DrugEntity,Integer>{
 
 }
-
--- src/main/java/com/bindingdai/repository/PhysicalExamRepository.java
+++ src/main/java/com/bindingdai/repository/PhysicalExamRepository.java
@@ -1,13 +1,14 @@
 package com.bindingdai.repository;
 
+import com.bindingdai.model.PhysicalExamEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.bindingdai.model.*;
+import com.bindingdai.model.SymptomEntity;
 /**
  * Created by daibinding on 16/4/19.
  */
 @Repository
-public interface DrugRepository extends JpaRepository<DrugEntity,Integer>{
+public interface PhysicalExamRepository extends JpaRepository<PhysicalExamEntity,Integer>{
 
 }
 
